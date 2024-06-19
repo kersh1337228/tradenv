0775c83c6a5e070ce5396de296bc48c7f63cf6ed
kersh1337228 <antonycherevko@gmail.com>
2024-06-19 04:41:43 +0300

    feat(general): - unnecessary files deleted.

--- client/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-
-ReactDOM.createRoot(
-    document.getElementById('content') as HTMLElement
-).render(
-    <App />
-)
--- client/src/types/strategy.ts
+++
@@ -1,7 +0,0 @@
-import {ArgsType} from "./general";
-
-export interface StrategyType {
-    verbose_name: string,
-    alias: string,
-    args: ArgsType
-}
